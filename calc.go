ce1bd12018a4ba3780df402aacce87d86b98ff06
Anthony Hallak <ahall012@ucr.edu>
2020-06-28 22:40:35 -0700

    converts CSV string correctly

--- calc.go
+++ calc.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func getData() ([]int, error) {
+func getData() ([]int64, error) {
 	userData := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter the sample data as comma separated values or whitespace separated values: ")
 	str, readErr := userData.ReadString('\n')
@@ -27,12 +28,22 @@ func getData() ([]int, error) {
 		strSplit = strings.Split(str, " ")
 	}
 
-	fmt.Printf("%q", strSplit)
-	toRet := []int{1}
+	var toRet []int64
+	for _, i := range strSplit {
+		toAdd, convErr := strconv.ParseInt(i, 10, 64)
+
+		if convErr != nil {
+			fmt.Printf("Error converting %q to 64 bit base 10 integer", i)
+			return []int64{}, convErr
+		}
+		toRet = append(toRet, toAdd)
+	}
+
 	return toRet, nil
 }
 
-func printStats(data []int) error {
+func printStats(data []int64) error {
+	fmt.Printf("Statistics for %q", data)
 	return nil
 }
 
